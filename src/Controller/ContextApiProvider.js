becfa64adcd502109695894250b8d1d41487629c
sanavulla shaik <sanavullashaik0@gmail.com>
2023-10-07 16:35:38 +0530

    Revert "branch check"
    This reverts commit a885427c89898939ffbf81caca3662225ae2a249.

--- src/Controller/ContextApiProvider.js
+++ src/Controller/ContextApiProvider.js
@@ -5,7 +5,7 @@ export const CounterContext = createContext(null);
 export const CounterProvider = (props) =>{
      const [count, setCount] = useState(0);
 
-
+  console.log(count )
 
   return (<CounterContext.Provider value={{count, setCount}} >
        {props.children}
