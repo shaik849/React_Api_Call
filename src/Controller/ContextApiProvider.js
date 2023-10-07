5d1e3613edc9e48d60061e3d516355e37b3ab631
sanavulla shaik <sanavullashaik0@gmail.com>
2023-10-07 16:33:29 +0530

    Revert "wrong commit"
    This reverts commit 922dc5609718e50d86df3d408c32ba35fb5cdbd8.

--- src/Controller/ContextApiProvider.js
+++ src/Controller/ContextApiProvider.js
@@ -5,11 +5,7 @@ export const CounterContext = createContext(null);
 export const CounterProvider = (props) =>{
      const [count, setCount] = useState(0);
 
-<<<<<<< HEAD
 
-=======
-  console.log(count)
->>>>>>> parent of f47ee59 (trail)
 
   return (<CounterContext.Provider value={{count, setCount}} >
        {props.children}
